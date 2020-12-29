920347714c7ef0b9b286d17a670d4efea4b4cd07
mjarnicki <37507193+mjarnicki@users.noreply.github.com>
2020-12-30 00:49:23 +0100

    preserve selection and unit tests missing

--- plugins/tablesort/plugin.js
+++ plugins/tablesort/plugin.js
@@ -1,81 +1,130 @@
-CKEDITOR.plugins.add( 'tablesort', {
-    lang: 'en,pl',
-    icons: 'tablesort',
+( function() {
 
-    init: function( editor ) {
+    function ckeckAndSetSortingDirection(table) {
+        // By setting below attribute it is possible to save sorting direction in each table. 
+        var sortingAttribute = 'data-sort-ascending'; 
+    
+        if(table.hasAttribute(sortingAttribute)) {
+            table.removeAttribute(sortingAttribute);
+            return false;
+        } else {
+            table.setAttribute(sortingAttribute, '');
+            return true;
+        }
+    }
+    
+    function createArrayFromCellValue(value) {
+        return value.trim().split(/([0-9,.]+)/).filter(elem => elem != '')
+    }
+    
+    function isNumber(string) {
+        // It was possible to check if string is a number inside a condition, but I decided to export it to a function in order to increase readability.
+        return /^\d+$/.test(string);
+    }
 
-        var sortAscending = true;
+    function returnTableCell(editor) {
+        // After sorting keyboard cursor is focused on 'body'. Because of that it is necessary to check and correct context of that cursor. 
+        var startElement = editor.getSelection().getStartElement()
 
-        editor.addCommand( 'sortTable', {
-            exec: function( editor ) {
+        if ( startElement.$.tagName === 'TBODY' ) {
+            return startElement.getElementsByTag('td').getItem(0)
+        } else {
+            return startElement;
+        }
+    }
 
-                sortTable(sortAscending)
-                sortAscending = !sortAscending;
+    function setStateOfIcon(editor) {
+        var style = new CKEDITOR.style( { element: 'table' } ),
+            command = editor.getCommand( 'tablesort' );
 
+        editor.attachStyleStateChange( style, function( state ) {
+            if ( state === 1 ){
+                command.setState( CKEDITOR.TRISTATE_OFF )
+            } else {
+                command.setState( CKEDITOR.TRISTATE_DISABLED )
             }
-        });
+        } );
+    }
 
-        editor.ui.addButton( 'TableSort', {
-            label: editor.lang.tablesort.label,
-            command: 'sortTable',
-            toolbar: 'insert'
-        });
+    var sortTable = {
 
-        var sortTable = function( sortAscending ){
-            var selection = editor.getSelection();
-            var element = selection.getStartElement();
+        exec: function( editor ) {
+            var tableCell = returnTableCell(editor),
+                columnNumber = tableCell.getAscendant( { td:1 }, true ).getIndex(),
+                table = tableCell.getAscendant({table:1}),
+                tbody = table.getElementsByTag('tbody').getItem(0),
+                items = tbody.$.childNodes,
+                itemsArr = []
+                isSortingAscending = ckeckAndSetSortingDirection(table);
 
-            if ( element ){
-                var columnNumber = element.getAscendant( { td:1 }, true ).getIndex();
-                var table = element.getAscendant({table:1});
-                var tbody = table.getElementsByTag('tbody').getItem(0);
-
-                if(tbody == undefined) tbody = table;
-
-                var items = tbody.$.childNodes;
-                var itemsArr = [];
-                for (var i in items) {
-                    if (items[i].nodeType == 1)
-                        itemsArr.push(items[i]);		
+            for ( var i in items ) {
+                if ( items[i].nodeType == 1 && items[i].querySelector('td')) {
+                    itemsArr.push(items[i]);		
                 }
+            }
 
-                itemsArr.sort(function(a, b) {
+            itemsArr.sort(function(a, b) {
 
-                    var aValue = a.childNodes[columnNumber].innerText.trim(); 
-                    var bValue = b.childNodes[columnNumber].innerText.trim(); 
+                var aArray = createArrayFromCellValue(a.childNodes[columnNumber].innerText),
+                    bArray = createArrayFromCellValue(b.childNodes[columnNumber].innerText);
+
+                for (var i in aArray) {
                     
-                    var aArray = aValue.split(/([0-9,.]+)/).filter(elem => elem != '')
-                    var bArray = bValue.split(/([0-9,.]+)/).filter(elem => elem != '')
+                    // Check if strings or numbers are equal. If yes, don't compare.
+                    if ( aArray[i] !== bArray[i] && 
+                        aArray[i] - bArray[i] !== 0 ) {
 
-
-                    for (var i in aArray) {
-                        // Check if strings or numbers are equal. If yes, don't compare.
-                        if(aArray[i] != bArray[i] && aArray[i] - bArray[i] != 0) {
-
-                            if(aArray[i]-0 + '' === 'NaN' && bArray[i]-0 + '' === 'NaN') {
-                                
-                                if(sortAscending === true) {
-                                    return aArray[i].localeCompare(bArray[i], 'en-US-u-kf-upper')
-                                } else {
-                                    return bArray[i].localeCompare(aArray[i], 'en-US-u-kf-upper')
-                                }
-
+                        if ( isNumber(aArray[i]) && isNumber(bArray[i]) ) {
+                            if ( isSortingAscending === true ) {
+                                return aArray[i] - bArray[i]
+                            }
+                            else {
+                                return bArray[i] - aArray[i]
+                            }
+                        } else {
+                            if (isSortingAscending === true) {
+                                return aArray[i].localeCompare(bArray[i], 'en-US-u-kf-upper')
                             } else {
-                                if(sortAscending === true) {
-                                        return aArray[i] - bArray[i]
-                                    }
-                                else {
-                                    return bArray[i] - aArray[i]
-                                }
+                                return bArray[i].localeCompare(aArray[i], 'en-US-u-kf-upper')
                             }
                         }
                     }
-                });
-
-                for (i = 0; i < itemsArr.length; ++i) {
-                  tbody.$.appendChild(itemsArr[i]);
                 }
+                return 0
+            });
+
+            for ( var i = 0; i < itemsArr.length; ++i ) {
+                tbody.$.appendChild(itemsArr[i]);
             }
-        }
+
+        },
+
+        startDisabled: true
     }
-});
+
+    CKEDITOR.plugins.add( 'tablesort', {
+        lang: 'en,pl',
+        icons: 'tablesort',
+
+        init: function( editor ) {
+
+            editor.addCommand( 'tablesort', sortTable );
+
+            setStateOfIcon(editor);
+
+            editor.ui.addButton( 'TableSort', {
+                label: editor.lang.tablesort.label,
+                command: 'tablesort',
+                toolbar: 'insert'
+            });
+
+            editor.setKeystroke( [
+                [ CKEDITOR.CTRL + 40 /*↓*/, 'tablesort' ]
+            ] );
+        
+        }
+    });
+
+} )();
+
+
